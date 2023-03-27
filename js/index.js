99e3a639e1110b06959f3fd56c1f3a8861109106
dhiraj <dk5987466@gmail.com>
2023-03-27 14:49:44 +0530

    this is video play website

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let menuIcon = document.querySelector(".menu-icon");
+let sidebar = document.querySelector(".sidebar");
+let container = document.querySelector(".container");
+
+
+
+menuIcon.onclick = function() {
+    sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
+}
